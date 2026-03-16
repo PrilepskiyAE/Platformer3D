8d8c301113f1478fabe187711ba6affcc69017c6
Prilepskiy_AE <alexey.prilepskiy1992@gmail.com>
2026-03-17 00:21:29 +0700

    Jump and animation graph

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _friction;
     [SerializeField] private float _jumpSpeed;
+    private bool grounded=true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +17,32 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            _rb.AddForce(0,_jumpSpeed,0,ForceMode.VelocityChange);
+        }
     }
 
     private void FixedUpdate()
     {
         _rb.AddForce(Input.GetAxis("Horizontal")*_moveSpeed,0,0,ForceMode.VelocityChange);
          _rb.AddForce(-_rb.linearVelocity.x*_friction,0,0,ForceMode.VelocityChange);
+        
+    }
+    
+
+    void OnCollisionStay(Collision collision)
+    {
+        float angle = Vector3.Angle(collision.contacts[0].normal,Vector3.up);
+        if (angle < 45f)
+        {
+            grounded = true;
+        }
+        
+
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        grounded=false;
     }
 }
