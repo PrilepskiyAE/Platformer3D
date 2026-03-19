afe38c1e30ad8dd6ec3baa754bfac47a71d96b72
Prilepskiy_AE <alexey.prilepskiy1992@gmail.com>
2026-03-19 23:07:38 +0700

    point

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,21 @@
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+   [SerializeField] private Transform aim;
+   [SerializeField] private Camera playerCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin,ray.direction*50,Color.yellow);
+        Plane plane = new Plane(-Vector3.forward,Vector3.zero);
+
+        float distance;
+        plane.Raycast(ray,out distance);
+        Vector3 point = ray.GetPoint(distance);
+        Debug.Log(distance);
+        aim.position = point;
+    }
+}
